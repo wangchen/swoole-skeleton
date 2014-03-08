c13a40e6f932849da083f636ee9eb5d4a5f0c438
wangchen <kitta4587@gmail.com>
2014-03-08 14:08:33 +0800

    完善粘包的处理

--- helpers/MessageHelper.php
+++ helpers/MessageHelper.php
@@ -77,43 +77,87 @@ class MessageHelper
 	 **/
 	public static function parse($input, $buf_get, $buf_set)
 	{
+		$head_length = 12;
+		echo "Enter!\n";
 		$log = Logger::getLogger('MessageHelper');
 		$messages = array();
-		$buf = '';
-		$read = self::string_io($input);
+		$buf = ''; // MUST no head here
+		$read = self::string_reader($input);
 		while(true)
 		{
-			$head_bytes = $read(12);
-			if ($head_bytes === false)
+			echo "LOOP\n";
+			$current_bytes = $read($head_length);
+			if ($current_bytes === false)
 			{
 				break;
 			}
-			$head = self::parse_head($head_bytes);
+			$head = self::parse_head($current_bytes);
 			if ($head === false)
 			{
-				$buf .= $head;
+				$buf .= $current_bytes;
 			} else {
-				if (strlen($buf) <> 0) {
+				// process the last message
+				if (strlen($buf) <> 0) { 
 					$last_buf = $buf_get();
-					$buf_set(NULL);
 					if (empty($last_buf))
 					{
+						// current data ($buf + $current_bytes) has no head
 						$log->warn("Illegal data: " . bin2hex($buf));
 					} else {
-						// handle the HEAD + BODY
-						$msg = $self::parse_msg($last_buf . $buf);
-						if ($msg) $messages[] = $msg;
+						$msg = $self::parse_msg($last_buf . $buf . $current_bytes);
+						// All data will be dropped if parse failed 
+						if (gettype($msg) === 'array') $messages[] = $msg;
 					}
-
+					// Clean buffers
+					$buf_set(NULL);
+					$buf = '';
 				} 
+				// process the current message
 				$body = $read($head['msg_len']);
 				if ($head['msg_len'] === strlen($body)) {
 					$messages[] = self::make_msg_obj($head, $body);
 				} else {
-					$buf_set($head_bytes . $body);
+					$buf .= $current_bytes . $body;
 				}
+			} // end if
+		} // end while
+
+		// Finally check buffers
+		if (strlen($buf) <> 0) {
+			$bytes = $buf_get() . $buf;
+			$head = self::parse_head(substr($bytes, 0, $head_length));
+			if ($head === false)
+			{
+				// No head, empty gbuf
+				$buf_set(null);
+			} else {
+				$whole_len = $head['msg_len'] + $head_length;
+				if (strlen($bytes) < $whole_len)
+				{
+					// a incomplete message
+					echo "a incomplete message\n";
+					$buf_set($bytes);
+				} else if (strlen($bytes) >= $whole_len) {
+					$msg = self::parse_msg(substr($bytes, 0, $whole_len));
+					if (gettype($msg) === 'array') {
+						// a complete message
+						echo "a complete message\n";
+						$messages[] = $msg;
+						$buf_set(null);
+					} else {
+						// This msg is corrupt
+						echo "This msg is corrupt\n";
+						$buf_set(null);
+					} 
+					if (strlen($bytes) > $whole_len) 
+					{
+						echo "Store the rest bytes, length: $whole_len\n";
+						$buf_set(substr($bytes, $whole_len));
+					}
+				}
+
 			}
-		}
+		} 
 		return $messages;
 	}
 
@@ -122,13 +166,19 @@ class MessageHelper
 		return array($head['msg_type'], $body);
 	}
 
+	// TODO: should get more return value
+	// - -1 : illegal msg
+	// - not complete msg
+	// - message
 	public static function parse_msg($msg)
 	{
 		$head = self::parse_head(substr($msg, 0, 12));
-		if ($head === false // No head
-			|| ($head['msg_len'] <> strlen($msg) -12) // Length not match
-		) {
-			return false;
+		if ($head === false) { // No head
+			return -1;
+		} else if ($head['msg_len'] <> strlen($msg) -12) {
+			// todo: fix
+			// Length not match
+			return -2;
 		}
 		return self::make_msg_obj($head, substr($msg, 12));
 	}
@@ -157,7 +207,7 @@ class MessageHelper
 		}
 		return $str;
 	}
-	public static function string_io($input)
+	public static function string_reader($input)
 	{
 		$pos = 0;
 		$len = strlen($input);
--- test/MessageHelperTest.php
+++ test/MessageHelperTest.php
@@ -10,7 +10,7 @@ require __DIR__ . '/../helpers/MessageHelper.php';
  **/
 class MessageHelperTest extends PHPUnit_Framework_TestCase
 {
-    public function testParseMsg()
+    public function testParseMsgBasic()
     {
         $input = pack('c4I2a*', ord('m'), ord('r'), ord('c'), ord('h'), 11, 2, "hello world");
         echo 'INPUT:' . bin2hex($input) . "\n";
@@ -25,17 +25,106 @@ class MessageHelperTest extends PHPUnit_Framework_TestCase
 
     }
 
+    public function testParseMsgOneMessageInTwoFrames()
+    {
+        $cache = null;
+        $msg_get = function() use (&$cache) {
+            return $cache;
+        };
+        $msg_set = function($buf) use (&$cache){
+            $cache = $buf;
+        };
+        // part 1
+        $input = pack('c4I2a*', ord('m'), ord('r'), ord('c'), ord('h'), 11, 2, "hello");
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        // incomplete message
+        $this->assertEquals(strlen($input), strlen($cache));
+        // Get nothing, things stored in 
+        $this->assertEquals(0, count($msg_array));
+        $input = pack('a*', ' world');
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        $this->assertEquals(1, count($msg_array));
+    }
+
+    public function testParseMsgTwoMessagesInThreeFrames1()
+    {
+        $cache = null;
+        $msg_get = function() use (&$cache) {
+            return $cache;
+        };
+        $msg_set = function($buf) use (&$cache){
+            $cache = $buf;
+        };
+        $magic_bytes = pack("c4", ord('m'), ord('r'), ord('c'), ord('h'));
+        // Frame 1
+        $input = $magic_bytes . pack('I2a*', 13, 2, "hello");
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        // incomplete message
+        $this->assertEquals(strlen($input), strlen($cache)); // len: 17
+        // Get nothing, things stored in 
+        $this->assertEquals(0, count($msg_array));
+
+        // Frame 2
+        $input = pack('a*', ' world 1') . $magic_bytes;
+        // The 1st complete msg
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        echo "cache: " . strlen($cache) . "\n";
+        $this->assertEquals(strlen($magic_bytes), strlen($cache));
+        $this->assertEquals(1, count($msg_array));
+
+        // Frame 3
+        $input = pack('I2a*', 13, 2, "hello world 2");
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        // The 1st complete msg
+        $this->assertEquals(null, $cache);
+        $this->assertEquals(1, count($msg_array));
+    }
+
+    public function testParseMsgTwoMessagesInThreeFrames2()
+    {
+        $cache = null;
+        $msg_get = function() use (&$cache) {
+            return $cache;
+        };
+        $msg_set = function($buf) use (&$cache){
+            $cache = $buf;
+        };
+        $magic_bytes = pack("c4", ord('m'), ord('r'), ord('c'), ord('h'));
+        // Frame 1
+        $input = $magic_bytes . pack('I2a*', 13, 2, "hello");
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        // incomplete message
+        $this->assertEquals(strlen($input), strlen($cache)); // len: 17
+        // Get nothing, things stored in 
+        $this->assertEquals(0, count($msg_array));
+
+        // Frame 2
+        $input = pack('a*', ' world 1') . $magic_bytes. pack("I2", 13, 2);
+        // The 1st complete msg
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        echo "cache: " . strlen($cache) . "\n";
+        $this->assertEquals(strlen($magic_bytes) + 8, strlen($cache));
+        $this->assertEquals(1, count($msg_array));
+
+        // Frame 3
+        $input = pack('a*', "hello world 2");
+        $msg_array = MessageHelper::parse($input, $msg_get, $msg_set);
+        // The 1st complete msg
+        $this->assertEquals(null, $cache);
+        $this->assertEquals(1, count($msg_array));
+    }
+
     public function testStringIO()
     {
-        $read = MessageHelper::string_io("0123456789");
+        $read = MessageHelper::string_reader("0123456789");
         echo $read(5)."\n";
         echo $read(6)."\n";
         echo $read(6)."\n";
 
-        $read = MessageHelper::string_io("0123456789");
+        $read = MessageHelper::string_reader("0123456789");
         echo $read(10)."\n";      
         
-        $read = MessageHelper::string_io("0123456789");
+        $read = MessageHelper::string_reader("0123456789");
         echo $read(-1)."\n";   
     }
 } // END class MessageHelperTest extends PHPUnit_Framework_TestCase
